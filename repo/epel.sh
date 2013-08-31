b3e86abe6ef8502d77ed913f1ccba78361387c3e
Adam Mallul <RaveMaker@users.noreply.github.com>
2013-08-31 17:36:50 +0300

    Update epel.sh

--- repo/epel.sh
+++ repo/epel.sh
@@ -1,4 +1,1 @@
-cd /opt/
-wget http://mirrors.nl.eu.kernel.org/fedora-epel/6/i386/epel-release-6-7.noarch.rpm
-rpm -Uvh epel-release-6-7.noarch.rpm
-rm epel-release-6-7.noarch.rpm -f
+rpm -Uvh http://mirrors.nl.eu.kernel.org/fedora-epel/6/i386/epel-release-6-7.noarch.rpm
